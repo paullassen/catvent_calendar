9c1ca6731831b917cb7ed3081505bb572b07ffa4
Paul Lassen <paul@ologicinc.com>
2022-12-23 14:40:53 -0800

    merging test program into main

--- main.py
+++ main.py
@@ -1,10 +1,62 @@
 #! /usr/bin/env python3
 
+import sys
+import os
+
+imgdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'img')
+libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib')
+
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
+import logging
+from waveshare_epd import epd5in65f
+import time
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+from datetime import date
+
 from lib import weather
 
 def main():
     w = weather.Weather("Sacramento,CA,US", "imperial")
-    print(f'{w.get_weather()}')
+
+    try:
+        logging.info("epd5in65f Demo")
+        
+        epd = epd5in65f.EPD()
+        logging.info("init and Clear")
+        epd.init()
+        #epd.Clear()
+        font24 = ImageFont.truetype(os.path.join(imgdir, 'Font.ttc'), 24)
+        font18 = ImageFont.truetype(os.path.join(imgdir, 'Font.ttc'), 18)
+        font40 = ImageFont.truetype(os.path.join(imgdir, 'Font.ttc'), 40)
+    
+        Himage = Image.open(os.path.join(imgdir, '5in65f0.png'))
+        Himage = Himage.resize((epd.width, epd.height), Image.ANTIALIAS)
+        draw = ImageDraw.Draw(Himage)
+
+        date_msg = date.today().strftime("%A, %d %B %Y")
+        w, h = draw.textsize(date_msg, font=font40)
+        draw.text(((epd.width-w)/2,0), date_msg, font = font40, fill=epd.BLACK)
+
+        weather_msg = w.get_weather()
+        w, h = draw.textsize(weather_msg, font=font40)
+        draw.text(((epd.width-w)/2,epd.height-h), date_msg, font = font40, fill=epd.BLACK)
+
+        epd.display(epd.getbuffer(Himage))
+        #time.sleep(60)
+        #epd.Clear()
+        epd.sleep()
+    
+    
+    except IOError as e:
+        logging.info(e)
+        
+    except KeyboardInterrupt:    
+        logging.info("ctrl + c:")
+        epd5in65f.epdconfig.module_exit()
+        exit()
 
 if __name__ == '__main__':
     main()
