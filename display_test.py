9a36b2c179e8f08afeb8ca401891cb493414f438
Paul Lassen <paul@ologicinc.com>
2022-12-24 16:56:10 -0800

    added display_test

---
+++ display_test.py
@@ -0,0 +1,51 @@
+#! /usr/bin/env python3
+
+import sys
+import os
+
+imgdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'img')
+libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib')
+
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
+import logging
+from waveshare_epd import epd5in65f
+import time
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+from datetime import date
+import random
+
+from lib import weather
+
+def main(location):
+    print("Init Weather")
+
+    try:
+        logging.info("epd5in65f Demo")
+        
+        epd = epd5in65f.EPD()
+        logging.info("init and Clear")
+        epd.init()
+
+        dirs = [('/'+d+'/') for d in os.listdir(imgdir) if os.path.isdir(dir + '/' + d)]
+        for d in dirs:
+            idir = imgdir + d
+            for i in os.listdir(idir):
+                print(idir+i)
+                img = Image.open(idir+i)
+                img = img.resize((epd.width, epd.height), Image.ANTIALIAS)
+                epd.display(epd.getbuffer(img))
+                time.sleep(30)
+
+    except IOError as e:
+        logging.info(e)
+        
+    except KeyboardInterrupt:    
+        logging.info("ctrl + c:")
+        epd5in65f.epdconfig.module_exit()
+        exit()
+
+if __name__ == '__main__':
+    main(sys.argv[1])
